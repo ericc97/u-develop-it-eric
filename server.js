dd7dc2bbc088bdcc5de3e4334c3df17a9ca7d5b3
Eric Coleman <ericcoleman9217@gmail.com>
2021-12-23 18:01:44 -0600

    connected to election database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,9 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySql username,
+        user: 'root',
+        // your MySql password
+        password: '9217Hurleycove!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
-// Default response for any other request (Not Found)
+// Default response for any other request (Not Found) aka catchall route
 app.use((req,res)  => {
     res.status(404).end()
 });
